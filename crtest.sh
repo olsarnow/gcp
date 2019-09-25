db1b8fb3e4e575de7c0bde0e8bddea9dbcaf3454
Olaf Sarnow <olsarnow@gmail.com>
2019-09-25 22:41:04 +0200

    chef docker added

--- crtest.sh
+++ crtest.sh
@@ -2,10 +2,15 @@
 
 image=ubuntu-1904
 #image=centos-7
-size=n1-highcpu-8
-nodename=test1
+#size=n1-highcpu-8
+size=n1-standard-4
+nodename=docker
 family=ubuntu-1904
 project=ubuntu-os-cloud
 
 
 gcloud compute instances create $nodename --image-family=$family --image-project=$project --machine-type=$size
+
+sleep 30
+
+gcloud compute instances reset $nodename
